d1426d49c01e726f782d9c38ccc1585fb218a76a
John <john@Ark.local>
2015-10-19 00:18:06 +0800

    Made some optimisation to the code; reused certain functions. Controller ID is no longer hardcoded at 0, script now detects controller id(s) and create vdisks(raid 0) out of unused pdisks.

--- dell_autocreate_vdisk.py
+++ dell_autocreate_vdisk.py
@@ -1,76 +1,66 @@
 #!/usr/bin/env python
 import shlex, subprocess
 
-pdisk_list = "omreport storage pdisk controller=0 -fmt ssv"
-vdisk_list = "omreport storage vdisk controller=0 -fmt ssv"
+controller_list = "omreport storage controller -fmt ssv"
+pdisk_list = "omreport storage pdisk controller={0} -fmt ssv"
+vdisk_list = "omreport storage vdisk controller={0} -fmt ssv"
+pdisk_of_used_vdisks_list = "omreport storage pdisk vdisk={1} controller={0} -fmt ssv"
 
 id_column = "awk -F';' '{ if (NR > 5) {print $1} }'"
-device_column = "awk -F';' '{ if (NR > 6) {print $11} }'"
+device_column = "awk -F';' '{ if (NR > 5) {print $11} }'"
 
-def get_vdisks_ids(arg1, arg2):
-    #gets list of virtual disk IDs
-    get_vdisks_list = subprocess.Popen(shlex.split(arg1), stdout = subprocess.PIPE)
-    get_vdisks_ids = subprocess.Popen(shlex.split(arg2), stdin = get_vdisks_list.stdout, stdout = subprocess.PIPE).communicate()[0]
-    get_vdisks_list.stdout.close()
-    get_vdisks_list.wait()
-    return get_vdisks_ids
+def get_omreport_info(arg1, arg2, arg3 = '0'):
+    # Get list of physical disk IDs.
+    # Reused to get list of virtual disks ID and device names.
+    print "get_omreport_info {0} {1} {2}".format(arg1, arg2, arg3)
+    get_list = subprocess.Popen(shlex.split(arg1.format(arg3)), stdout = subprocess.PIPE)
+    get_columns = subprocess.Popen(shlex.split(arg2), stdin = get_list.stdout, stdout = subprocess.PIPE).communicate()[0]
+    get_list.stdout.close()
+    get_list.wait()
+    #print get_columns
+    return get_columns
 
-def get_pdisks_ids(arg1, arg2):
-    #gets list of physical disk IDs
-    get_pdisks_list = subprocess.Popen(shlex.split(arg1), stdout = subprocess.PIPE)
-    get_pdisk_ids = subprocess.Popen(shlex.split(arg2), stdin = get_pdisks_list.stdout, stdout = subprocess.PIPE).communicate()[0]
-    get_pdisks_list.stdout.close()
-    get_pdisks_list.wait()
-    return get_pdisk_ids
-
-def get_vdisks_device_names(arg1, arg2):
-    #get list of device names of virtual disks
-    get_vdisks_list = subprocess.Popen(shlex.split(arg1), stdout = subprocess.PIPE)
-    vdisks_device_names_list = subprocess.Popen(shlex.split(arg2), stdin = get_vdisks_list.stdout, stdout = subprocess.PIPE).communicate()[0]
-    get_vdisks_list.stdout.close()
-    get_vdisks_list.wait()
-    return vdisks_device_names_list
-
-def create_vdisks(arg1):
+def create_vdisks(arg1, arg3 = '0'):
     #get list of disks in /dev/ for partitioning
-    command = "omconfig storage controller controller=0 action=createvdisk raid=r0 size=max pdisk="
-    command1 = []
+    #print "create_vdisks {0} {1}".format(arg1, arg3)
+    command = "omconfig storage controller action=createvdisk raid=r0 size=max pdisk={0} controller={1}"
     for i in arg1:
-        command1.append(command + i)
-    for i in command1:
-        createvdisk = subprocess.Popen(shlex.split(i))
+        createvdisk = subprocess.Popen(shlex.split(command.format(i, arg3)))
         createvdisk.wait()
 
 def partition_vdisks(arg1):
     #partitioning of virtual disks
     for i in arg1:
-        command = "/sbin/fdisk " + i
+        #print "partition_vdisks {0}".format(i)
+        command = "/sbin/fdisk {0}".format(i)
         with open('/tmp/fdisk_choices') as f:
             partition_devices = subprocess.Popen(shlex.split(command), stdin = f, stdout = subprocess.PIPE).communicate()[0]
         f.close()
-        command = "/sbin/fdisk "
 
-vdisk_ids_inused = get_vdisks_ids(vdisk_list, id_column).split()
+#get list of controller ids
+controller_ids = get_omreport_info(controller_list, id_column).split()
 
-pdisk_ids_inused = []
+#for each controller, the script identify used vdisks - and corresponding pdisks - and left alone, the remaining pdisks are then created as a raid0 vdisk.
+for i in controller_ids:
+    used_vdisks = get_omreport_info(vdisk_list, id_column, i).split()
+    all_pdisk_id = get_omreport_info(pdisk_list,id_column, i).split()
 
-for i in vdisk_ids_inused:
-    _list_of_pdisks_based_on_vdisks_inused = "omreport storage pdisk vdisk={0} controller=0 -fmt ssv".format(i)
-    pdisk_of_vdisk = subprocess.Popen(shlex.split(_list_of_pdisks_based_on_vdisks_inused), stdout = subprocess.PIPE)
-    id_pdisk_of_vdisk = subprocess.Popen(shlex.split(id_column), stdin = pdisk_of_vdisk.stdout, stdout = subprocess.PIPE).communicate()[0]
-    id_pdisk_of_vdisk = id_pdisk_of_vdisk.strip().split()
-    for j in id_pdisk_of_vdisk:
-        pdisk_ids_inused.append(j)
+    used_pdisks = []
 
-all_pdisk_id = get_pdisks_ids(pdisk_list,id_column).split()
+    for j in used_vdisks:
+        pdisk_of_vdisk = subprocess.Popen(shlex.split(pdisk_of_used_vdisks_list.format(i, j)), stdout = subprocess.PIPE)
+        id_pdisk_of_vdisk = subprocess.Popen(shlex.split(id_column), stdin = pdisk_of_vdisk.stdout, stdout = subprocess.PIPE).communicate()[0]
+        id_pdisk_of_vdisk = id_pdisk_of_vdisk.strip().split()
+        for k in id_pdisk_of_vdisk:
+            used_pdisks.append(k)
+        #print used_pdisks
 
-cache_and_fast_disks_pdisks = []
-for k in all_pdisk_id:
-    if k not in pdisk_ids_inused:
-        cache_and_fast_disks_pdisks.append(k)
+    cache_and_fast_disks_pdisks = []
+    for l in all_pdisk_id:
+        if l not in used_pdisks:
+            cache_and_fast_disks_pdisks.append(l)
+    #print cache_and_fast_disks_pdisks
 
-create_vdisks(cache_and_fast_disks_pdisks)
-
-cache_fast_device_names = get_vdisks_device_names(vdisk_list, device_column).split()
-
-partition_vdisks(cache_fast_device_names)
+    create_vdisks(cache_and_fast_disks_pdisks)
+    cache_fast_device_names = get_omreport_info(vdisk_list, device_column).split()
+    partition_vdisks(cache_fast_device_names)
